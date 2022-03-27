83f6a7784b6540f6fb898f72ee67983c0b0d4e33
Dmitry <65062995+Dimkabit@users.noreply.github.com>
2022-03-27 21:40:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ const choices = new Choices(choicesElem, {
 
 const getData = (error, url) => {
 	return fetch(url, {
-		method: 'POST', // *GET, POST, PUT, DELETE, etc.
+		method: 'GET', // *GET, POST, PUT, DELETE, etc.
 		mode: 'cors', // no-cors, *cors, same-origin
 		cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
 		credentials: 'same-origin', // include, *same-origin, omit
